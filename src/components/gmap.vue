80636ce7d9a84cb1af237f96c71fa54b80523a12
Jeamar <jeamar1234@gmail.com>
2020-03-01 17:13:21 +0800

    updates

--- src/components/gmap.vue
+++ src/components/gmap.vue
@@ -10,12 +10,17 @@
 				google: null,
 				mapConfig: {
 					center: {lat: -34.397, lng: 150.644},
-          zoom: 12
+          zoom: 17
 				},
 				apiKey: config.gmapApiKey,
 				infoWindow: null,
 				map : null,
 				marker : null,
+				locations : [
+					['Jollibee Divisoria', 8.477828, 124.644370],
+		      ['Cafe La Tea', 8.475854, 124.645205],
+		      ['Bank of the Philippine Islands', 8.478838, 124.644544],
+				]
 			}
 		},
 		async mounted() {
@@ -45,7 +50,7 @@
 	    },
 	    getGeoLocation(){
 	    	console.log( this.map );
-	    	var infoWindow = new google.maps.InfoWindow;
+	    	var infoWindow = [];
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition((position) => {
@@ -53,15 +58,43 @@
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            // infoWindow.open(this.map);
-            this.marker = new this.google.maps.Marker({
-					    position: pos,
-					    map: this.map,
-					    title: ''
-					  });
+
+					  this.marker = new this.google.maps.Marker({
+			        position: new google.maps.LatLng(pos.lat, pos.lng),
+			        map: this.map
+			      });
+			      this.google.maps.event.addListener(this.marker, 'click', ((marker, i) => {
+			        return () => {
+			          infoWindow[0].setContent('I am here.');
+			          infoWindow[0].open(this.map, marker);
+			        }
+			      })(this.marker, i));
+			      infoWindow[0] = new google.maps.InfoWindow;
+			      infoWindow[0].setContent('I am here.');
+			      infoWindow[0].open(this.map, this.marker);
+
+				    var i;
+
+				    for (i = 0; i < this.locations.length; i++) {  
+				      this.marker = new this.google.maps.Marker({
+				        position: new google.maps.LatLng(this.locations[i][1], this.locations[i][2]),
+				        map: this.map
+				      });
+
+				      this.google.maps.event.addListener(this.marker, 'click', ((marker, i) => {
+				        return () => {
+				          infoWindow[i + 1].setContent(this.locations[i][0]);
+				          infoWindow[i + 1].open(this.map, marker);
+				        }
+				      })(this.marker, i));
+				      infoWindow[i + 1] = new google.maps.InfoWindow;
+				      infoWindow[i + 1].setContent(this.locations[i][0]);
+				      infoWindow[i + 1].open(this.map, this.marker);
+				    }
+
+
             this.map.setCenter(pos);
+            
           },() => {
             this.handleLocationError(true, infoWindow, this.map.getCenter());
           });
